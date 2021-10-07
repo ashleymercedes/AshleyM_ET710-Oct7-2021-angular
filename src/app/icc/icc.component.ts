81b3b52b0a44dd6c29dc9869fce8548ff10cbf34
ashleymercedes <ashley.mercedes64@login.cuny.edu>
2021-10-07 12:00:58 -0400

    1 file modified, 2 added

---
+++ src/app/icc/icc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icc',
+  templateUrl: './icc.component.html',
+  styleUrls: ['./icc.component.css']
+})
+export class ICCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
